e60710aca20bb0483f6077c9619f924ae36e4678
Balázs Burján <bigbali141@gmail.com>
2023-02-18 23:48:12 +0200

    deleted old pathfinder folder

--- apps/example-projects/src/components/_pages/algorithms/pathfinding/state.ts
+++
@@ -1,8 +0,0 @@
-
-// TypeScript doesn't like this being in the correct place, so we have to move it elsewhere (here).
-// Basically, it's a circular reference thing.
-export enum PathfinderState {
-    STOPPED = 'stopped',
-    RUNNING = 'running',
-    PAUSED = 'paused'
-};
