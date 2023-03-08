7a82d890f83ab99113c06243a4591347a3f66cac
Balázs Burján <bigbali141@gmail.com>
2023-03-08 23:31:25 +0200

    module federation

--- apps/example-messages/next.config.mjs
+++ apps/example-messages/next.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
@@ -8,6 +10,31 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
+    webpack(config, options) {
+        const { isServer } = options;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        config.plugins.push(
+            new NextFederationPlugin({
+                // @ts-ignore
+                name: 'example-messages',
+                remotes: {
+                    examples: `examples@http://localhost:3000/_next/static/${
+                        isServer ? 'ssr' : 'chunks'
+                    }/remoteEntry.js`
+                },
+                filename: 'static/chunks/remoteEntry.js',
+                exposes: {
+                    './messages': './src/pages/messages.tsx'
+                },
+                shared: {
+                    // whatever else
+                }
+            })
+        );
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return config;
+    },
     reactStrictMode: false,
     /**
      * If you have the "experimental: { appDir: true }" setting enabled, then you
