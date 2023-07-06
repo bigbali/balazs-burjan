96b90e29f5d387e30c0e4f49ab716fbf9690676d
Balázs Burján <bigbali141@gmail.com>
2023-07-06 17:59:26 +0300

    fixed trpc using wrong port when using nextjs rewrite from master page

--- packages/messages/src/utils/api.ts
+++ packages/messages/src/utils/api.ts
@@ -14,7 +14,6 @@ import { type AppRouter } from '../server/api/root';
 import BASEPATH from './basepath.mjs';
 
 const getBaseUrl = () => {
-    if (typeof window !== 'undefined') return '/project/messages'; // browser should use relative url
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
     return `http://localhost:${process.env.PORT ?? 3002}${BASEPATH}`; // dev SSR should use localhost
 };
